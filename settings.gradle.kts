5a8819299c6e1026a25c536d630bd2f8705a2d01
Elmarabtii <elmarabtiayoub600@gmail.com>
2025-11-21 17:48:20 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }   // 🔥 AJOUT IMPORTANT
+        // jcenter()  // tu peux le supprimer, il ne sert plus
+    }
+}
+
+
+rootProject.name = "SOAPCompteApp"
+include(":app")
